08f56fba07994740c1051280ef854bd386a999a2
richierockskool <38402006+richierockskool@users.noreply.github.com>
2022-06-09 13:40:49 -0400

    Update settings.ts

--- src/settings.ts
+++ src/settings.ts
@@ -6,4 +6,4 @@ export const PLATFORM_NAME = 'InkbirdWiFiGatewayHomebridgePlugin';
 /**
  * This must match the name of your plugin as defined the package.json
  */
-export const PLUGIN_NAME = 'homebridge-plugin-name';
+export const PLUGIN_NAME = 'homebridge-InkBirdIBS-M1';
