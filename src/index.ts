df8b510c50cd4617742c337360dadfc01a352bc8
richierockskool <38402006+richierockskool@users.noreply.github.com>
2022-06-11 14:20:45 -0400

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,11 @@
 import { API } from 'homebridge';
 
-import { PLATFORM_NAME = 'InkbirdWiFiGatewayHomebridgePlugin' } from './settings';
+import { PLATFORM_NAME } from './settings';
 import { ExampleHomebridgePlatform } from './platform';
 
 /**
  * This method registers the platform with Homebridge
  */
 export = (api: API) => {
-  api.registerPlatform(PLATFORM_NAME = 'InkbirdWiFiGatewayHomebridgePlugin' , ExampleHomebridgePlatform);
+  api.registerPlatform(PLATFORM_NAME, ExampleHomebridgePlatform);
 };
