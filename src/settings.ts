00a8384adf05a04541e5579125e610072584d70f
richierockskool <38402006+richierockskool@users.noreply.github.com>
2022-06-20 17:12:31 -0400

    Update on plugin

--- src/settings.ts
+++ src/settings.ts
@@ -6,4 +6,4 @@ export const PLATFORM_NAME = 'homebridge-inkbird-wifi-gateway-platform';
 /**
  * This must match the name of your plugin as defined the package.json
  */
-export const PLUGIN_NAME = 'homebridge-inkbird-wifi-gateway';
+export const PLUGIN_NAME = 'homebridge-inkbird-wifi-gateway-platform';
