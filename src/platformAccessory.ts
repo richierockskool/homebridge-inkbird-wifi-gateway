a8ded867a2084deffb00dca9c463f0c960d0a5c2
richierockskool <38402006+richierockskool@users.noreply.github.com>
2022-06-20 16:08:53 -0400

    Update platformAccessory.ts

--- src/platformAccessory.ts
+++ src/platformAccessory.ts
@@ -8,7 +8,7 @@ import { HomebridgeInkbirdWifiGatewayPlatform } from './platform';
  * An instance of this class is created for each accessory your platform registers
  * Each accessory may expose multiple services of different service types.
  */
-export class TemperatureSensorAccessory {
+export class HomebridgeTemperatureSensorAccessory {
   private service: Service;
 
   /**
