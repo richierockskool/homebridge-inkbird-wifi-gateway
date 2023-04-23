0349f5bc5197671a19d697f96ba2d48fbe21d74c
richierockskool <38402006+richierockskool@users.noreply.github.com>
2023-04-23 19:08:40 -0400

    Update index.js

--- src/index.js
+++ src/index.js
@@ -65,13 +65,7 @@
 // variables have to be declared explicitly
 'use strict';
 
-// eslint-disable-next-line @typescript-eslint/no-var-requires, no-undef
-const packageJson = require('./package.json');
 
-// eslint-disable-next-line no-console, no-undef
-console.log(packageJson);
-// eslint-disable-next-line no-console, no-undef
-console.log(packageJson.homebridge-inkbird-wifi-gateway);
 
 //-----------------------------------------------------------------------
 // Imports
