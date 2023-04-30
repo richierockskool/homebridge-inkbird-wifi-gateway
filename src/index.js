db59252d241385144caa379831a6ed16a8fd7b51
richierockskool <38402006+richierockskool@users.noreply.github.com>
2023-04-30 13:40:32 -0400

    Update index.js

--- src/index.js
+++ src/index.js
@@ -4,5 +4,5 @@ import HomebridgeInkbirdWifiGateway from './InkbirdPlatform.js';
 export default (homebridge) => {
   // eslint-disable-next-line no-undef
   global.homebridge = homebridge;
-  homebridge.registerPlatform(HomebridgeInkbirdWifiGateway);
+  homebridge.registerPlatform('homebridge-inkbird-wifi-gateway', HomebridgeInkbirdWifiGateway);
 };
