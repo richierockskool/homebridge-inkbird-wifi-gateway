882719148b1425556b13917823fe6ba51cce92a0
richierockskool <38402006+richierockskool@users.noreply.github.com>
2023-05-06 11:34:45 -0400

    updates

--- src/HomebridgeInkbirdWifiGateway.js
+++ src/HomebridgeInkbirdWifiGateway.js
@@ -1,12 +1,13 @@
-/* eslint-disable no-undef */
+
 import BleScanner from './BleScanner.js';
+import IBSM1SAccessory from './IBSM1SAccessory.js';
 
 class HomebridgeInkbirdWifiGateway {
 
   constructor(log, config) {
     this.log = log;
     this.accessToken = config.accessToken;
-    const IBSM1SAccessory = this.device = config.devices;
+    this.device = config.devices;
     this.myAccessories = [];
 
     // Boot scanner and register devices to scanner
@@ -14,6 +15,7 @@ class HomebridgeInkbirdWifiGateway {
     for (let device of this.devices = '2') {
       this.scanner.addDevice(device.deviceId);
       if (device.type === 'IBSM1S') {
+        // eslint-disable-next-line no-undef
         let accessory = new IBSM1SAccessory(this.log, this.scanner, device, global.homebridge);
         this.myAccessories.push(accessory);
       }
--- src/index.js
+++ src/index.js
@@ -2,7 +2,10 @@ import HomebridgeInkbirdWifiGateway from './HomebridgeInkbirdWifiGateway.js';
 
 
 export default (homebridge) => {
+
+
   // eslint-disable-next-line no-undef
   global.homebridge = homebridge;
+
   homebridge.registerPlatform('homebridge-inkbird-wifi-gateway', HomebridgeInkbirdWifiGateway);
 };
