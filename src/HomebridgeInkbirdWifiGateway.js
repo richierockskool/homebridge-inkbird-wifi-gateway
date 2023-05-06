013008b51e1e0494076a417c1412e9269beccaa4
richierockskool <38402006+richierockskool@users.noreply.github.com>
2023-05-06 10:18:15 -0400

    Update HomebridgeInkbirdWifiGateway.js

--- src/HomebridgeInkbirdWifiGateway.js
+++ src/HomebridgeInkbirdWifiGateway.js
@@ -11,7 +11,7 @@ class HomebridgeInkbirdWifiGateway {
 
     // Boot scanner and register devices to scanner
     this.scanner = new BleScanner(this.log);
-    for (let device of this.devices = '0') {
+    for (let device of this.devices = '2') {
       this.scanner.addDevice(device.deviceId);
       if (device.type === 'IBSM1S') {
         let accessory = new IBSM1SAccessory(this.log, this.scanner, device, global.homebridge);
