77a0c48276a24e353d0581d97904fcdf1e8c24dd
richierockskool <38402006+richierockskool@users.noreply.github.com>
2023-07-28 09:42:51 -0400

    eslint update

--- src/InkbirdPlatform.js
+++ src/InkbirdPlatform.js
@@ -15,6 +15,18 @@ export class InkbirdPlatform {
     this.myAccessories = [];
     this.api=api;
 
+    this.log.debug('Finished initializing platform:', this.config.name);
+
+    // When this event is fired it means Homebridge has restored all cached accessories from disk.
+    // Dynamic Platform plugins should only register new accessories after this event was fired,
+    // in order to ensure they weren't added to homebridge already. This event can also be used
+    // to start discovery of new accessories.
+    this.api.on('didFinishLaunching', () => {
+      log.debug('Executed didFinishLaunching callback');
+      // run the method to discover / register your devices as accessories
+      
+    });
+  
 
 
 
